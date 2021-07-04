9aa0bd6a64673fd38f0817d4c98dfe5cc554928b
Scott Prosser <scott.prosser@att.net>
2021-07-04 18:32:32 -0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }   
+        }
+    }
+}
